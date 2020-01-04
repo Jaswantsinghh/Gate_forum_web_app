f56f88299c273fe8cd9ea73d76dbcb3d46e046cc
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-04 22:10:12 +0530

    Create forgot.php

---
+++ forgot.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Forgot password</title>
+        <link rel="stylesheet" type="text/css" href="instyle.css">
+    </head>
+    <body>
+        <div class="loginbox">
+        <img src="image/dribbble.jpg" class="avtar">
+        <h1>Retrive Password</h1>
+        <form method="post" action="forgotpass.php">
+                <input type="text" name="user" placeholder="Enter username" required>
+                <br>
+                <input type="text" name="sq" placeholder="Your's First Pet name" required>
+                <br>
+                <input type="submit" name="submit" value="Continue">
+        </form>   
+        </div>
+    </body>
+</html>
