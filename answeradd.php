281de46390870b4f217f5fcc060a8af2c7fb6d8f
Vaibhav Shukla <vaibhav2017shukla@gmail.com>
2020-01-04 19:08:02 +0530

    Update answeradd.php

--- answeradd.php
+++ answeradd.php
@@ -1,18 +1,6 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Gatelogin";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    echo "unsussessful connection";
-}
-
+include 'database.php';
 $name = $_POST['id'];
 $ans = $_POST['sol'];
 $time = $_POST['tm'];
