b6ab7113101b8a2d24e2097332b4e90ef8e6f0f0
Vaibhav Shukla <vaibhav2017shukla@gmail.com>
2020-01-04 19:09:06 +0530

    Update confermation.php

--- confermation.php
+++ confermation.php
@@ -1,19 +1,7 @@
 <?php
 
 session_start();
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Gatelogin";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+include 'database.php';
 $name = $_POST['user'];
 $pass = $_POST['pass'];
 
