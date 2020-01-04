17719b5a19e60254f0c70144b520fcaa5e5bb5fa
PriyanshuMay <54521023+PriyanshuMay@users.noreply.github.com>
2020-01-04 18:06:33 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,28 +1,24 @@
 <?php
 session_start();
-
 header('location:signin.php');
 
 include("database.php");
-
+ 
 $name = $_POST['user'];
 $pass = $_POST['pass'];
+$mob  = $_POST['mobile'];
+$secu  = $_POST['security'];
 
-$sql = "INSERT INTO signin (username,password)
-VALUES ('$name', '$pass')";
-
+$sql = "INSERT INTO signin (username,password,mobile,security)
+VALUES ('$name', '$pass','$mob','$secu')";
 $q = "select * from signin where username = '$name' && password = '$pass'";
-
 $result = mysqli_query($conn,$q);
-
 $num = mysqli_num_rows($result);
-
 if ($num == 1) {
 	echo "Try any other username or password";
 } else {
-	$qy = "insert into signin(username,password) values ('$name','$pass')";
+	$qy = "insert into signin(username,password) values ('$name','$pass','$mob','$secu')";
 	mysqli_query($conn,$qy);
 	 
 }
-
 ?>
