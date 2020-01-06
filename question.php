e77adab5f9223f2acc3ec6330753adef0d45f75e
Vaibhav Shukla <vaibhav2017shukla@gmail.com>
2020-01-06 18:19:19 +0530

    Update question.php

--- question.php
+++ question.php
@@ -3,22 +3,22 @@
     <head>
     
         <mata charset= "utf-8"></mata>
-        <title> GATE </title>
+        <title> GATE FORUM </title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     </head>
     <body style="background-color: #7FFF00">
           <br><br><br><br><br><br>
-        <div class="container text-center ">
-            <h1> GATE QUESTIONS</h1>
-            <div class="jumbotron">
-                <h3>Add New Question </h3>
+        <div class="container text-center">
+            <h1 style="color: blue;"><u><b> GATE QUESTIONS</b></u></h1>
+            <div class="jumbotron" style="background-color: black;">
+                <h3 style="color: red;">Add New Question </h3>
                 <form method="post" action="addquestion.php">
                     <div class ="form-group">
-                       <lable>Question</lable>
+                       <lable style="color: green;">Question</lable>
                         <input type="text" name="ques" class="form-control" placeholder="Enter your question here" required>
                     </div>
-                 <!--   <div class ="form-group">
+                   <!-- <div class ="form-group">
                         <lable>Your Name</lable>
                         <input type="text" name="nm" class="form-control" placeholder="Enter your name" required>
                     </div>
