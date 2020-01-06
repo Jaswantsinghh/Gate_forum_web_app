41582bcbb3570614aa8b03737fee7e52b0e37576
Vaibhav Shukla <vaibhav2017shukla@gmail.com>
2020-01-06 13:52:02 +0530

    Update addquestion.php

--- addquestion.php
+++ addquestion.php
@@ -1,14 +1,30 @@
 <?php
 
 include 'database.php';
+
+session_start();
+
+$u = $_SESSION['username'];
+
+$sql = "SELECT name, email FROM signin WHERE username = '$u'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) 
+        while($row = $result->fetch_assoc()) {
+        	$qname = $row["name"];
+			$qmail = $row["email"];
+
+        }
+
+
 $question = $_POST['ques'];
-$qname = $_POST['nm'];
-$qmail = $_POST['mail'];
+//$qname = $_POST['nm'];
+//$qmail = $_POST['mail'];
 
 $sql = "INSERT INTO questionaddition(que,name,email) VALUES ('$question','$qname','$qmail')";
 
 if ($conn->query($sql) === TRUE) {
-    $last_id = $conn->insert_id;
+   // $last_id = $conn->insert_id;
     header('location:home.php');
     //echo "New record created successfully. Last inserted ID is: " . $last_id;
 } else {
